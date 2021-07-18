84fbc4e58e33dbecd41e3edbd5bfb04cf4ca8051
Gian F. Olivieri <gian.olivieri@gmail.com>
2021-07-18 19:11:07 -0300

    adds dat.gui

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import debounce from 'lodash.debounce'
 import * as THREE from 'three'
+import * as dat from 'dat.gui'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import './style.css'
 
@@ -8,6 +9,11 @@ const canvas = document.querySelector('[data-canvas]') as HTMLCanvasElement
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight
+}
+
+const gui = new dat.GUI()
+const debugObject = {
+  color: '#FF00CC'
 }
 
 const onResize = () => {
@@ -27,11 +33,18 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 
 camera.position.set(2, 2, -2)
 scene.add(camera)
 
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: '#FF00CC' })
-)
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: debugObject.color })
+
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
 scene.add(cube)
+
+gui.addColor(debugObject, 'color').name('Color').onChange(() => {
+  cubeMaterial.color = new THREE.Color(debugObject.color)
+})
+gui.add(cube.rotation, 'x').min(0).max(Math.PI * 2).step(0.01).name('RotationX')
+gui.add(cube.rotation, 'y').min(0).max(Math.PI * 2).step(0.01).name('RotationY')
+gui.add(cube.rotation, 'z').min(0).max(Math.PI * 2).step(0.01).name('RotationZ')
 
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
@@ -48,3 +61,8 @@ const tick = () => {
 
 window.addEventListener('resize', debounce(onResize, 100))
 tick()
+
+if (import.meta.env.PROD) {
+  gui.hide()
+  window.showGUI = () => gui.show()
+}
